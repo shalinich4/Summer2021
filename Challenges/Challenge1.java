349df52d70abd3ff18591f70c9b4813e31d6bccc
ranai-srivastav <sweetynranai@gmail.com>
2021-08-10 00:35:11 -0500

    Day 1 and Challenge 1

---
+++ Challenges/Challenge1.java
@@ -0,0 +1,20 @@
+package Challenges;
+
+/**
+ * Level 1: Write a program that accepts 2 numbers from the user and calculates
+ * the sum, difference, product and quotient of these numbers.
+ * 
+ * Level 2: 
+ */
+public class Challenge1
+{
+    // USE THIS SYNTAX TO ACCEPT INPUT FROM THE USER
+    // Scanner sc new Scanner(System.in);
+    // int i = sc.nextInt();
+    // double d = sc.nextDouble();
+    
+    public static void main(String args[])
+    {
+        
+    }
+}
