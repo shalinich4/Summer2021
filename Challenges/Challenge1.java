cb2a2d6d5b78bd6fc729532ab679d78a079aeb67
shalinich4 <88741190+shalinich4@users.noreply.github.com>
2021-08-12 12:14:43 -0500

    Shalini challenge 1 updated

--- Challenges/Challenge1.java
+++ Challenges/Challenge1.java
@@ -1,5 +1,5 @@
 package Challenges;
-
+import java.util.Scanner;
 /**
  * Level 1: Write a program that accepts 2 numbers from the user and calculates
  * the sum, difference, product and quotient and remainder of these numbers.
@@ -9,13 +9,25 @@ package Challenges;
  */
 public class Challenge1
 {
-    // USE THIS SYNTAX TO ACCEPT INPUT FROM THE USER
-    // Scanner sc new Scanner(System.in);
-    // int i = sc.nextInt();
-    // double d = sc.nextDouble();
+    // initializing values for i and d from input
+    
     
     public static void main(String args[])
     {
-        
+    Scanner sc=new Scanner(System.in);
+    // initializing values for i and d from input
+    int i = sc.nextInt();
+    double d = sc.nextDouble();
+    // performing arithmetic operations on i and d 
+    double sum=i+d;
+    System.out.println("sum of i and d :"+sum);
+    double diff=i-d;
+    System.out.println("difference of i and d :"+diff);
+    double prod=i*d;
+    System.out.println("product of i and d :"+prod);
+    double div=i/d;
+    System.out.println("division of i and d :"+div);
+    double mod=i%d;
+    System.out.println("modulo of i and d :"+mod);  
     }
 }
---
+++ Day2_VariablesDatatypes.java
@@ -0,0 +1,38 @@
+
+/**
+ * Write a description of class Day2_VariablesDatatypes here.
+ *
+ * @author (shalini chandupatla)
+ * @version (August 12, 2021)
+ */
+public class Day2_VariablesDatatypes
+{
+    // instance variables - replace the example below with your own
+    private int x;
+
+    /**
+     * Constructor for objects of class Day2_VariablesDatatypes
+     */
+    public Day2_VariablesDatatypes()
+    {
+        // initialise instance variables
+        x = 10;
+        System.out.println("Assigned value of x to 10");
+        System.out.print("testing");
+        System.out.print("testing 2");
+    }
+
+    /**
+     * An example of a method - replace this comment with your own
+     *
+     * @param  y  a sample parameter for a method
+     * @return    the sum of x and y
+     */
+    public int sampleMethod(int y)
+    {
+        // put your code here
+        System.out.println("returning x+y");
+        return x + y;
+        
+    }
+}
